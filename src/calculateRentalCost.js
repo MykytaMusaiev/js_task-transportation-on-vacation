aacff1d03b356846291edf093d2ccc2c159ce663
Mykyta Musaiev <n.musaiev@gmail.com>
2025-07-07 15:44:13 +0300

    changed up to checklist requirments

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,14 @@
  */
 function calculateRentalCost(days) {
   const daylyRentSum = 40;
+  const shortTermDiscount = 20;
+  const longTermDiscount = 50;
 
   return days === 1 || days === 2
     ? days * daylyRentSum
     : days >= 7
-      ? days * daylyRentSum - 50
-      : days * daylyRentSum - 20;
+      ? days * daylyRentSum - shortTermDiscount
+      : days * daylyRentSum - longTermDiscount;
 }
 
 module.exports = calculateRentalCost;
